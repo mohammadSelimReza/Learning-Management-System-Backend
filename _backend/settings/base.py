36d67410c12c9fe2f2dc0b06c6cf4d525aaa268d
mohammadSelimReza <srreza1999@gmail.com>
2025-02-12 16:46:47 +0600

    vercelJson again

--- _backend/settings/base.py
+++ _backend/settings/base.py
@@ -28,11 +28,12 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1",".vercel.app"]
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:8000",
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:5173",
+#     "http://127.0.0.1:8000",
+# ]
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = []
 # Application definition
 
 INSTALLED_APPS = [
